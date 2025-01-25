997e9909badb4e8d332aa630293442333ed30bdc
weixu <xuwei19850423@gmail.com>
2025-01-25 13:34:04 +1100

    instantiate recyclerView

--- app/src/main/java/com/example/todo/MainActivity.kt
+++ app/src/main/java/com/example/todo/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.todo
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : ComponentActivity() {
+    private lateinit var recyclerView: RecyclerView  // Declare RecyclerView with lateinit
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerView = findViewById(R.id.recView)
     }
 }
