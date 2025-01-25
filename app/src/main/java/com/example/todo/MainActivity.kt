412570f3dec00fe6ad6dd1e981eaea1622726876
weixu <xuwei19850423@gmail.com>
2025-01-25 14:01:46 +1100

    add linearLayoutManager into recyclerView

--- app/src/main/java/com/example/todo/MainActivity.kt
+++ app/src/main/java/com/example/todo/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.todo
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : ComponentActivity() {
@@ -12,5 +14,14 @@ class MainActivity : ComponentActivity() {
         setContentView(R.layout.activity_main)
 
         recyclerView = findViewById(R.id.recView)
+
+        val linearLayoutManager = LinearLayoutManager(this)
+        linearLayoutManager.orientation = LinearLayoutManager.VERTICAL
+        val dividerItemDecoration = DividerItemDecoration(this, linearLayoutManager.orientation)
+        recyclerView.addItemDecoration(dividerItemDecoration)
+
+        recyclerView.layoutManager = linearLayoutManager
+
+
     }
 }
