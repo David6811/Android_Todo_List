f383a8ae0b337df63b985c5ae03668ba57b4a43c
David <86584824+David6811@users.noreply.github.com>
2025-01-26 15:45:18 +1100

    Create item (#6)
    * Add a floatingActionButton
    
    * Add a createItem activity
    
    * Implement creating new Item

--- app/src/main/java/com/example/todo/dao/AppDatabase.kt
+++ app/src/main/java/com/example/todo/dao/AppDatabase.kt
@@ -1,10 +1,30 @@
 package com.example.todo.dao
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.todo.entities.Icons
 
 @Database(entities = [Icons::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun iconsDao(): IconDao  // Provides access to the IconDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        // Singleton pattern to get the database instance
+        fun getInstance(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "todo_database"
+                ).allowMainThreadQueries().build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
 }
